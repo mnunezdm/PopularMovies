069022fa27b29681c2371f7213e7d6a8b746cdac
Miguel Núñez Díaz-Montes <mnunezdm@gmail.com>
2017-02-08 20:44:02 +0100

    Activity Detailed Movie recreated

--- app/src/main/java/com/example/migui/popularmovies/NetworkUtils.java
+++ app/src/main/java/com/example/migui/popularmovies/NetworkUtils.java
@@ -1,36 +1,14 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.migui.popularmovies;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
-import android.util.Log;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
-import java.util.logging.Logger;
 
 class NetworkUtils {
     private final static String FILM_BASE_URL =
@@ -41,6 +19,7 @@ class NetworkUtils {
 
     private final static String KEY = "YOUR_API_KEY_GOES_HERE"; // TODO api key!!
 
+    // TODO use https://github.com/square/okhttp
     static String queryFilms(String sort) throws IOException {
         Uri.Builder uriBuilder = Uri.parse(FILM_BASE_URL).buildUpon()
                 .appendPath(sort).appendQueryParameter("api_key", KEY);
