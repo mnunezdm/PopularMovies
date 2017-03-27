5af77ad4bd7e2b2a8968ccd88edb5eaaa5e014b4
Miguel Núñez Diaz-Montes <mnunezdm@gmail.com>
2017-03-28 00:31:33 +0200

    Fixed FC at start

--- app/src/main/java/com/example/migui/popularmovies/data/MovieDBHelper.java
+++ app/src/main/java/com/example/migui/popularmovies/data/MovieDBHelper.java
@@ -9,7 +9,7 @@ public class MovieDBHelper extends SQLiteOpenHelper{
 
     public static  final String DATABASE_NAME = "movie.db";
 
-    private static final int DATABASE_VERSION = 0;
+    private static final int DATABASE_VERSION = 1;
 
     public MovieDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
